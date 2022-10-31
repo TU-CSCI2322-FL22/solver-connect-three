9858f875f705f176f298d59f54770ab0759f2ab0
Colin Houston <chousto1@trinity.edu>
2022-10-31 13:21:40 -0500

    Slight changes (in class 10/31/22)

--- game.hs
+++ game.hs
@@ -1,4 +1,10 @@
 data Board = State State State State State State State State State
 data State = X | O | U
+data Play = Game Int Int State
 type Victory = State
 type Game = (Board, Victory)
+
+
+
+--makePlay :: Play -> Game
+checkWin :: [Game] -> [Game] 
