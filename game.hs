2cb7d808495961a56cb016d3f5d0ea83f36e275f
joey12725 <joeyandpaige@gmail.com>
2022-11-02 12:45:33 -0500

    added type signatures and macroboard type

--- game.hs
+++ game.hs
@@ -1,10 +1,29 @@
-data Board = State State State State State State State State State
 data State = X | O | U
 data Play = Game Int Int State
 type Victory = State
 type Game = (Board, Victory)
-
-
+data Board = Board State State State State State State State State State
+type Macroboard = Macroboard Board Board Board Board Board Board Board Board Board
 
 --makePlay :: Play -> Game
 checkWin :: [Game] -> [Game] 
+
+--Play -> Game Making a play on a specific tile of a specific board
+makePlay :: Play -> Game
+makePlay = undefined
+
+--Play -> Bool Checking if a given tile has been played
+checkPlay :: Play -> Bool
+checkPlay = undefined
+
+--Play -> Bool Check if a move is legal
+checkLegal :: Play -> Bool
+checkLegal = undefined
+
+--Macroboard -> Macroboard Checking every move if the individual game has been won - if so, then place a move on the big board
+checkWin :: Macroboard -> Macroboard
+checkWin = undefined
+
+--Macroboard -> Str Show function
+showMacroboard :: Macroboard -> Str
+showMacroboard = undefined
