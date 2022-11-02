40cd06da1e39ae4bcd10c78e8bed827107dae563
2021-08-24 <pharris@janus11.cs.trinity.edu>
2022-11-02 13:08:41 -0500

    "colin"

--- Main.hs
+++ Main.hs
@@ -5,29 +5,30 @@ type Game = (Board, Victory)
 data Board = Board State State State State State State State State State
 type Macroboard = Macroboard Board Board Board Board Board Board Board Board Board
 
---makePlay :: Play -> Game
-checkWin :: Board -> Bool
+-- Board positions are numbered as follows:
+-- 0  |  1  |  2
+-- ____________
+-- 3  |  4  |  5
+-- ____________
+-- 6  |  7  |  8
+
+checkWin :: Board -> State
 checkWin = undefined
 
-checkMacrowin :: Macroboard -> Bool
+checkMacrowin :: Macroboard -> State
 checkMacrowin = undefined
 
--- Making a play on a specific tile of a specific board
 makePlay :: Play -> Game
 makePlay = undefined
 
--- Checking if a given tile has been played
 checkPlay :: Play -> Bool
 checkPlay = undefined
 
--- Check if a move is legal
 checkLegal :: Play -> Bool
 checkLegal = undefined
 
--- Checking every move if the individual game has been won - if so, then place a move on the big board
 checkWin :: Macroboard -> Macroboard
 checkWin = undefined
 
--- Show function
 showMacroboard :: Macroboard -> Str
 showMacroboard = undefined
