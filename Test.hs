ed49bb057422af60f04eeabdd129b8b968ef7263
Colin Houston <chousto1@trinity.edu>
2022-11-07 20:44:10 -0600

    Wrote some test code, haven't really used it yet though

--- Test.hs
+++ Test.hs
@@ -22,3 +22,36 @@ testMacroboard   = [(microboardOne, Ongoing), (microboardTwo, Ongoing), (microbo
                    (microboardFour, Ongoing),(microboardFive, Ongoing), (microboardSix, Ongoing), 
                    (microboardSeven, Ongoing), (microboardEight, Ongoing), (microboardNine, Ongoing)]
 
+
+--the last thing before String in the type definitions is the expected result of the function
+testCheckWin :: Microboard -> Victory -> String
+testCheckWin micboard vic = 
+    if (checkWin micboard /= vic) then "checkWin error"
+    else "checkWin test passed"
+    
+testCheckMacrowin :: Macroboard -> Victory -> String
+testCheckMacrowin macboard vic =
+    if (checkMacrowin macboard /= vic) then "checkMacrowin error"
+    else "checkMacrowin test passed"
+
+testMakeMicroPlay :: Microgame -> Play -> Player -> Microgame -> String
+testMakeMicroPlay micgame play player newgame =
+    if (makeMicroPlay micgame play player) /= newgame then "makeMicroPlay error"
+    else "makeMicroPlay test passed"
+
+testMakeMacroPlay :: Macrogame -> Player -> Macrogame -> String
+testMakeMacroPlay macgame player newgame =
+    if (makeMacroPlay macgame player) /= newgame then "makeMacroPlay error"
+    else "makeMacroPlay test passed"
+
+testCheckPlay :: Play -> Microboard -> Bool -> String
+testCheckPlay play micboard expectedBool =
+    if (checkPlay play micboard) /= expectedBool then "checkPlay error"
+    else "checkPlay test passed"
+
+testCheckLegal :: Play -> Bool -> String
+testCheckLegal play expectedBool =
+    if (checkLegal play) /= expectedBool then "checkLegal error"
+    else "checkLegal test passed"
+
+
