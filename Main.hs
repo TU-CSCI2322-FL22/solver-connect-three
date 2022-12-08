7b675735cd26f1f547784debb89ff142c456256e
Colin Houston <chousto1@trinity.edu>
2022-12-07 18:41:51 -0600

    Fixed CSVs and reformatted things in Main, IO stuff now appears to be functional

--- Main.hs
+++ Main.hs
@@ -20,8 +20,8 @@ options :: [OptDescr Flag]
 options = [ Option ['w'] ["winner"] (NoArg Winner) "Print out the best move using an exhaustive search." 
           , Option ['d'] ["depth"] (ReqArg Depth "<num>") "Prints out the best move within a cutoff depth of potential future moves." 
           , Option ['h'] ["help"] (NoArg Help) "Prints usage information."
-          , Option ['m'] ["move"] (ReqArg Move "<move>") "Makes a move and prints out the resulting board, in the input format, to stdout. The move should be formatted with quotes around a tuple."
-          , Option ['v'] ["verbose"] (NoArg Verbose) "Outputs both the move and a description of how good it is (call with -m)."
+          , Option ['m'] ["move"] (ReqArg Move "<move>") "Makes a move and prints out the resulting board (the move should be formatted with quotes around a tuple)."
+          , Option ['v'] ["verbose"] (NoArg Verbose) "Outputs both the move and a description of how good it is (call with along with -m)."
           ]
 
 
@@ -49,28 +49,23 @@ main =
      then putStrLn $ usageInfo "Usage: Supreme Tic Tac Toe [options] [file]" options
      else do
              let fname = if null inputs then "ZZemptyboard.csv" else head inputs
-             fileString <- loadGame fname
-             let game = fileString
-             --let game = loadGame fname
+             game <- loadGame fname
              chooseAction flags game
-             --specifiedDepth game (getDepth flags)
-          --   Nothing -> 
-          --       case getMove flags game of
-          --           Just move -> makeMoveAndPrintBoard game move
-          --           Nothing -> chooseAction flags game
+
 
 chooseAction :: [Flag] -> Macrogame -> IO ()
 chooseAction flags game
-    | (Winner `elem` flags) = putStrLn $ show (bestPlay game)
+    | (Winner `elem` flags) = putStrLn $ "Best move (by exhaustive search): " ++ show (bestPlay game)
 --    | (Verbose `elem` flags) = verboseFunction game (fromJust $ getMove flags game)
     | otherwise = do case getMove flags game of
                        Nothing -> giveGoodMove game (getDepth flags)
                        Just move ->
                            if (Verbose `elem` flags) then
                                let scoreAfterPlay = scoreGame (makePlay move game)
+                                   formattedMove = ((fst move + 1), (snd move + 1))
                                in
-                                   if (scoreAfterPlay < 0) then putStrLn $ "The play " ++ show move ++ " is good for O with a score of " ++ show (-1 * scoreAfterPlay)
-                                   else putStrLn $ "The play " ++ show move ++ " is good for X with a score of " ++ show (scoreAfterPlay)
+                                   if (scoreAfterPlay < 0) then putStrLn $ "The play " ++ show formattedMove ++ " is good for O with a score of " ++ show (-1 * scoreAfterPlay) ++ "."
+                                   else putStrLn $ "The play " ++ show formattedMove ++ " is good for X with a score of " ++ show (scoreAfterPlay) ++ "."
                            else makeMoveAndPrintBoard game move
 
 hasMove :: Maybe Play -> (Bool, Play)
@@ -79,8 +74,6 @@ hasMove mplay =
         Nothing -> (False, (0,0))
         Just play -> (True, play) 
 
-  --exhaustiveDepth :: IO()
-  --exhaustiveDepth = putStrLn $ show (bestPlay game)
 
 --specifiedDepth :: Macrogame -> Int -> IO()
 --specifiedDepth game depth = putStrLn $ show (predictMightWin game depth)
@@ -89,16 +82,13 @@ makeMoveAndPrintBoard :: Macrogame -> Play -> IO()
 -- I hope you like the generous use of the dollar sign notation in this very line of code
 makeMoveAndPrintBoard game play = putStrLn $ showMacroboard $ fst $ makePlay play game
 
---verboseFunction :: Macrogame -> Play -> IO()
---verboseFunction game play =
--- I love backwards syntax; don't you??????????????????????
---        let scoreAfterPlay = scoreGame (makePlay play game)
---        in 
---                if (scoreAfterPlay < 0) then putStrLn $ "The play " ++ show play ++ " is good for O with a score of " ++ show (-1 * scoreAfterPlay) 
---                else putStrLn $ "The play " ++ show play ++ " is good for X with a score of " ++ show (scoreAfterPlay)
+
 
 giveGoodMove :: Macrogame -> Int -> IO()
-giveGoodMove game depth = putStrLn $ show $ decentPlay game depth
+giveGoodMove game depth = 
+    let preUIPlay = decentPlay game depth
+        play = ((fst preUIPlay + 1), (snd preUIPlay + 1))
+    in putStrLn $ "Likely best move: " ++ show play
 
 
 
